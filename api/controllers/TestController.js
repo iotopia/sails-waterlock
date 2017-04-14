d50d5513c86cf131b4e415122307b0767c72b8eb
Yo Vannaravuth <ravuthz@gmail.com>
2017-04-14 18:45:26 +0000

    Create TestController

---
+++ api/controllers/TestController.js
@@ -0,0 +1,21 @@
+/**
+ * TestController
+ *
+ * @description :: Server-side logic for managing tests
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	open: function(req, res) {
+	    res.ok(['This action is open']);
+	},
+	
+	auth: function(req, res) {
+	    res.ok(['This action is authenticated']);
+	},
+	
+	token: function(req, res) {
+        res.ok(['You have a Json Web Token (jwt)']);
+	}
+};
+
